69fd665b124e58532e689ee7bedb2160a243d229
Grept <tomjansen.408@gmail.com>
2022-04-18 18:15:46 +0200

    In the AuthController I added authorization to the exposedHeaders. Now the frontend can see the jwtToken response.

--- src/main/java/nl/tomjansen/loopgain/controller/api/AuthController.java
+++ src/main/java/nl/tomjansen/loopgain/controller/api/AuthController.java
@@ -5,6 +5,7 @@ import nl.tomjansen.loopgain.dto.model.user.AuthDto;
 import nl.tomjansen.loopgain.security.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(value = "http://localhost:3000/")
+@CrossOrigin(value = "http://localhost:3000/", exposedHeaders = "Authorization")
 public class AuthController {
 
     // Dependendy Injection
@@ -33,8 +34,15 @@ public class AuthController {
 
         UserDetails ud = (UserDetails) auth.getPrincipal();
         String token = jwtService.generateToken(ud);
-        return ResponseEntity.ok()
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
-                .body("Token generated");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+
+        System.out.println(token);
+        return new ResponseEntity<>("Token generated", headers, HttpStatus.OK);
+
+//        return ResponseEntity.ok()
+//                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+//                .body("Token generated");
     }
 }
